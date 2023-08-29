d222b1a2ce24baf1707b211ceeb916c4298a54e4
CJ-Codes <ccoreas484@gmail.com>
2023-08-29 14:22:31 -0400

    Scoreboard added

--- src/Components/GameScore/index.js
+++ src/Components/GameScore/index.js
@@ -1,5 +1,26 @@
+import { GetRedScore, GetYellowScore } from '../../const/selectors/scores';
+import './styles.css'
+
+
 const GameScore = () => {
-    return <h1>Score</h1>
+  const redTeamScore = GetRedScore();
+  const yellowTeamScore = GetYellowScore();
+
+  return (
+    <div className="gameScore">
+      <h1>Scores</h1>
+      <div className="teamScores">
+        <div className="score">
+          <h1>Red</h1>
+          <h2>{redTeamScore}</h2>
+        </div>
+        <div className="score">
+          <h1>Yellow</h1>
+          <h2>{yellowTeamScore}</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default GameScore;
