aa7b3c1277ead869e792ba2d7e36fe192759c5e8
CJ-Codes <ccoreas484@gmail.com>
2023-08-30 22:15:18 -0400

    updated variable names, formatted code

--- src/Components/BoardGame/index.js
+++ src/Components/BoardGame/index.js
@@ -1,39 +1,43 @@
 import React, { useState, useEffect } from "react";
-import _ from "lodash";
+import {range, findLastIndex} from "lodash";
+import { useDispatch } from "react-redux";
+
+import gameSetting from "../../const/gameSettings";
+import { increaseScore, changeTurn } from "../../Redux/game";
+import checkConsecutive from "../../utils/checkConsecutive.js";
+import { useTranslation } from "react-i18next";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
-import gameSetting from "../../const/gameSettings";
-import { useDispatch } from "react-redux";
-import { changeTurn } from "../../Redux/currentTurn";
-import { increaseScore } from "../../Redux/gameScores";
 
 const BoardGame = () => {
+  const { t } = useTranslation();
   const dispatch = useDispatch();
   const numRows = gameSetting.numRows;
   const numCols = gameSetting.numCols;
-  const [grid, setGrid] = useState(Array(numRows).fill(Array(numCols).fill(null)));
+  const [board, setBoard] = useState(Array(numRows).fill(Array(numCols).fill(null)));
   const [currentPlayer, setCurrentPlayer] = useState(gameSetting.players[0]);
   const [opposingPlayer, setOpposingPlayer] = useState(gameSetting.players[1]);
   const [winner, setWinner] = useState(null);
 
   const initGame = () => {
     setWinner(null);
-    setGrid(Array(numRows).fill(Array(numCols).fill(null)));
+    setBoard(Array(numRows).fill(Array(numCols).fill(null)));
   };
 
   const handleCellClick = (col) => {
     if (winner) return;
-    const updatedGrid = grid.map((row) => [...row]);
-    const emptyRow = _.findLastIndex(updatedGrid, (row) => !row[col]);
+    const updatedGrid = board.map((row) => [...row]);
+    const emptyRow = findLastIndex(updatedGrid, (row) => !row[col]);
 
     if (emptyRow !== -1) {
       updatedGrid[emptyRow][col] = currentPlayer;
-      setGrid(updatedGrid);
+      setBoard(updatedGrid);
     }
   };
 
   const renderCell = (row, col) => {
-    const value = grid[row][col];
+    const value = board[row][col];
     return (
       <div
         key={col}
@@ -46,7 +50,7 @@ const BoardGame = () => {
   const renderRow = (row) => {
     return (
       <div key={row} id={`row${row}`} className="row">
-        {grid[row].map((_, col) => (
+        {board[row].map((_, col) => (
           <div key={col} className="col">
             {renderCell(row, col)}
           </div>
@@ -55,32 +59,17 @@ const BoardGame = () => {
     );
   };
 
-  const checkConsecutive = (color, row, col, rowIncrement, colIncrement) => {
-    for (let i = 0; i < 4; i++) {
-      const newRow = row + i * rowIncrement;
-      const newCol = col + i * colIncrement;
-      if (
-        newRow < 0 ||
-        newRow >= numRows ||
-        newCol < 0 ||
-        newCol >= numCols ||
-        grid[newRow][newCol] !== color
-      ) {
-        return false;
-      }
-    };
-    return true;
-  };
-
   const winnerStatus = () => {
-    _.range(0, numRows).forEach((row) => {
-      _.range(0, numCols).forEach((col) => {
-        if (grid[row][col]) {
+    range(0, numRows).forEach((row) => {
+      range(0, numCols).forEach((col) => {
+        if (board[row][col]) {
+          //? Explanation for If Statement: checkConsecutive is designed to check in both vetical & horizontal directions.
+          //? If the function detects that their is any row or column with 4 of the same color in either direction. It will return TRUE
           if (
-            checkConsecutive(grid[row][col], row, col, 1, 0) ||
-            checkConsecutive(grid[row][col], row, col, 0, 1)
+            checkConsecutive(board, board[row][col], row, col, 1, 0) ||
+            checkConsecutive(board, board[row][col], row, col, 0, 1)
           ) {
-            setWinner(grid[row][col]);
+            setWinner(board[row][col]);
             dispatch(increaseScore({ key: `${currentPlayer}` }));
             return;
           }
@@ -94,14 +83,14 @@ const BoardGame = () => {
     setCurrentPlayer(currentPlayer === "red" ? "yellow" : "red");
     setOpposingPlayer(opposingPlayer === "yellow" ? "red" : "yellow");
     dispatch(changeTurn(opposingPlayer));
-  }, [grid]);
+  }, [board]);
 
   return (
     <div className="connect4-grid">
       {winner ? (
         <div className="winner-message">
-          <p>{winner === "red" ? "Red" : "Yellow"} player wins!</p>
-          <button onClick={() => initGame()}>New Game</button>
+          <p>{winner === "red" ? "Red" : "Yellow"} {t('winner.winnerStatement')}</p>
+          <button onClick={() => initGame()}>{t(`button.new`)}</button>
         </div>
       ) : (
         <div className="game-board">
