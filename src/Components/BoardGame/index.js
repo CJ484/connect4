9eb932c060bdd8a59946ade11bf4928252c9fbc5
CJ-Codes <ccoreas484@gmail.com>
2023-08-30 22:18:22 -0400

    updated eslint rule

--- src/Components/BoardGame/index.js
+++ src/Components/BoardGame/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 import {range, findLastIndex} from "lodash";
 import { useDispatch } from "react-redux";
